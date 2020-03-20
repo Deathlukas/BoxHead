c8b72b56b097b5d7491e604a035c8d507a92de7b
Deathlukas <lukasandersen15@gmail.com>
2020-03-20 11:49:56 +0100

    Tilføjet en skærm til spillet

--- Boxhead.py
+++ Boxhead.py
@@ -3,3 +3,23 @@ import time
 import random
 
 pygame.init()
+
+# Screen settings
+screen = pygame.display.set_mode((1200,768),pygame.FULLSCREEN)
+clock = pygame.time.Clock()
+# Function for the Screen
+def Screen():
+    clock.tick(50)
+    clock.tick(50)
+    screen.fill((255,255,255))
+
+
+
+
+# Her er mit game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
